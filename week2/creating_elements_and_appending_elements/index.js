a42ca9fb9c6c19fff1af1e659892b4a08702b97f
Daniel Segovia <58751893+DlSegovia@users.noreply.github.com>
2020-02-18 14:31:18 -0600

    week2 assignment creating&Appending_elements

---
+++ week2/creating_elements_and_appending_elements/index.js
@@ -0,0 +1,11 @@
+var newH1 = document.createElement("h1")
+newH1.textContent = "Welcome to my JS site"
+newH1.style.textAlign = "center"
+newH1.style.fontSize = "30px"
+document.body.append(newH1)
+
+var newP = document.createElement("p")
+newP.textContent = "All of this was created with Javascript"
+newP.style.textAlign = "center"
+newP.style.fontSize = "30px"
+document.body.append(newP)
