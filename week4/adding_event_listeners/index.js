e8a82f5ca38f4730be3a21e3508a60d4bb69eeb2
Daniel Segovia <58751893+DlSegovia@users.noreply.github.com>
2020-03-28 00:20:57 -0500

    week4 assignment

---
+++ week4/adding_event_listeners/index.js
@@ -0,0 +1,23 @@
+box1.addEventListener("mouseover", function() {
+    box1.style.backgroundColor = "blue"
+});
+
+box1.addEventListener("mousedown", function() {
+    box1.style.backgroundColor = "red"
+});
+
+box1.addEventListener("mouseup", function() {
+    box1.style.backgroundColor = "yellow"
+});
+
+box1.addEventListener("dblclick", function() {
+    box1.style.backgroundColor = "green"
+});
+
+box1.addEventListener("scroll", function() {
+    box1.style.backgroundColor = "orange"
+});
+
+box1.addEventListener("mouseout", function() {
+    box1.style.backgroundColor = "purple"
+});
